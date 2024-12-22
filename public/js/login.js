ca8fe2157c94e38b35ecd969baedf8fed8f4de82
BlueSky555 <egeserter2008@gmail.com>
2024-12-22 21:39:41 +0300

    fixed login

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,6 @@
 let submit = document.querySelector("#submit");
 let name = document.querySelector("#name");
 let secret = document.querySelector("#secret");
-let error = document.querySelector("#error");
 submit.onclick = () => {
     if(name.value && secret.value) fetch("/login?name=" + encodeURIComponent(name.value) + "&secret=" + encodeURIComponent(secret.value)).then(res => res.text()).then(res => {
         if(res == "1") location.href = "/";
